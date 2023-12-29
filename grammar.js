2fd52869873a06ef6e75f77abe319b6e5fc69f76
RubixDev <silas.groh@t-online.de>
2023-12-29 15:23:37 +0800

    chore: format code

--- grammar.js
+++ grammar.js
@@ -14,15 +14,16 @@ module.exports = grammar({
                 $.label,
                 $.instruction,
             ),
-        
-        meta: $ => seq(
-            field('kind', $.meta_ident),
-            optional(choice(
-                $.ident,
-                seq($.int, repeat(seq(',', $.int))),
-                seq($.float, repeat(seq(',', $.float))),
-            ))
-        ),
+
+        meta: $ =>
+            seq(
+                field('kind', $.meta_ident),
+                optional(choice(
+                    $.ident,
+                    seq($.int, repeat(seq(',', $.int))),
+                    seq($.float, repeat(seq(',', $.float))),
+                )),
+            ),
         label: $ => seq($.ident, ':', optional(seq('(', $.ident, ')'))),
         instruction: $ => seq(field('kind', $.word), sep(',', $._expr)),
 
